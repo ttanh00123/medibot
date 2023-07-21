7efa3ac3a54647ae90a97b0b0111ff792ca9f973
= <=>
2023-07-21 23:15:05 +0700

    ok

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('main.html')
+
+@app.route("/get", methods=["GET", "POST"])
+def respond():
+    msg = request.form["msg"]
+    #response = chatbot.get_response(msg)
+    response = "not there yet"
+    return str(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
